bbbd691e02d1fc63a0ef583f8d9366e68626e3af
HenryJimenezP <ing.henry.jimenez.95@hotmail.com>
2022-04-27 16:50:12 -0700

    Paso 4: Importando la clase ExplorerService a index.js

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
+const ExplorerService = require('./lib/services/ExplorerService');
 const Reader = require('./lib/utils/Reader')
+
 
 const explorers = Reader.readJsonFile("explorers.json");
 console.log(explorers)
+
+// Aplicación del ExplorerService sobre la lista de explorers
+ExplorerService.filterByMission(explorers, "node");
+ExplorerService.getAmountOfExplorersByMission(explorers, "node");
+ExplorerService.getExplorersUsernamesByMission(explorers, "node");
