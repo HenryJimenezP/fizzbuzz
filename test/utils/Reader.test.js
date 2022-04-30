30f1df3f4f78d1a5eeba7ecff163aaf5afee3afe
HenryJimenezP <ing.henry.jimenez.95@hotmail.com>
2022-04-29 20:15:43 -0700

    Update: Corrigiendo test del Reader

--- test/utils/Reader.test.js
+++ test/utils/Reader.test.js
@@ -3,6 +3,6 @@ const Reader =  require("../../lib/utils/Reader")
 describe ("Test para  Reader", () => {
     test("Requerimiento 1: Calcular todos los explorer en la mision", () => {
         const explorersTest = Reader.readJsonFile("test/data/explorer_test.js")
-        expect(explorersTest).toBe(2)
+        expect(explorersTest.length).toBe(2)
     })
 })
