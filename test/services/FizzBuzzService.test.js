99a8585e026bf22dd153933a38c84aaedf3aa24c
HenryJimenezP <ing.henry.jimenez.95@hotmail.com>
2022-04-30 15:12:39 -0700

    Agregando el requerimiento 1 a la prueba del score 1

---
+++ test/services/FizzBuzzService.test.js
@@ -0,0 +1,9 @@
+const FizzbuzzService = require("../../lib/services/FizzbuzzService")
+
+describe("Test para ExplorerService", () => {
+    test("Requerimiento 1: Uso de score 1", () => {
+        const explorer1 = {name: "Explorer1", score: 1}
+        const explorer = FizzbuzzService.applyValidationInExplorer(explorer1)
+        expect(explorer).toBe(1)
+    })
+})
