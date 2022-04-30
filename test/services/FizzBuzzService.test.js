64e876cf01f883fd53515f1c2e70dd7049d33b15
HenryJimenezP <ing.henry.jimenez.95@hotmail.com>
2022-04-30 15:34:27 -0700

    Update: Corrigiendo el requerimiento 1 a la prueba del score 1

--- test/services/FizzBuzzService.test.js
+++ test/services/FizzBuzzService.test.js
@@ -4,6 +4,6 @@ describe("Test para ExplorerService", () => {
     test("Requerimiento 1: Uso de score 1", () => {
         const explorer1 = {name: "Explorer1", score: 1}
         const explorer = FizzbuzzService.applyValidationInExplorer(explorer1)
-        expect(explorer).toBe(1)
+        expect(explorer).toStrictEqual({name: "Explorer1", score: 1, trick: 1})
     })
 })
